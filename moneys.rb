9cda25f8e17c3a4571b8986c135893c144a57e1f
Maciek Nowacki <nowacki@ualberta.ca>
2017-12-12 16:44:52 -0700

    no idea, I issued 'commit' before remembering the check the diff, heh

--- moneys.rb
+++ moneys.rb
@@ -11,7 +11,7 @@ qcx_thread = Thread.new do
 	quadrigacx_templ=URI "https://api.quadrigacx.com/v2/ticker?book=%s"
 
 	(
-		%w{ btc_cad eth_cad btg_cad ltc_cad }.each_with_object (
+		%w{ btc_cad eth_cad btg_cad ltc_cad bch_cad }.each_with_object (
 			(Net::HTTP.new quadrigacx_templ.host, quadrigacx_templ.port).tap do |http|
 				puts "qcx: connecting…"
 				http.use_ssl = true
@@ -55,35 +55,45 @@ end.map do |book, ticker_vals|
 	[ book, hilolt ]
 end
 
-btc_cad, eth_cad, btg_cad, ltc_cad = qcx_tickers.map &:last
+btc_cad, eth_cad, btg_cad, ltc_cad, bch_cad = qcx_tickers.map &:last
 
-costs_to_date = ARGV[0..-6].reduce 0 do |s,c| s += c.to_r end
-iota_held, btg_held, btc_held, eth_held, ltc_held = (-5..-1).map do |idx| Rational ARGV[idx], 100000000 end
+costs_to_date = ARGV[0..-7].reduce 0 do |s,c| s += c.to_r end
+iota_held, btg_held, btc_held, eth_held, ltc_held, bch_held = (-6..-1).map do |idx| Rational ARGV[idx], 100000000 end
 
 holdings_in_cad = btc_cad.last * btc_held +
 	eth_cad.last * eth_held +
 	btg_cad.last * btg_held +
 	ltc_cad.last * ltc_held +
+	bch_cad.last * bch_held +
 	btc_cad.last * iota_held * iota_in_btc
 
 printf "done!\n\n" +
 	"Okay, here's how it looksⓇ:\n\n" +
 	"\t* you have CAD $%s held in total\n" +
-		"\t\t* btc:\t$%s\n" +
-		"\t\t* eth:\t$%s\n" + 
-		"\t\t* btg:\t$%s\n" +
-		"\t\t* iota:\t$%s\n" +
-		"\t\t* ltc:\t$%s\n" +
+		(%w{ btc eth btg iota ltc bch }.map do |tck| 
+			"\t\t* #{tck}:\t$%s\n"
+		end.join) +
 	"\t* less costs of $%s…\n" +
 	"\t* …makes $%s earned to date\n" +
 	"\t* return as a percentage is %s%%.\n" +
 	"\nGreat Work™\n\n\n",
-	((format "%06i", holdings_in_cad*100000).insert -6,"."),
-	((format "%06i", btc_cad.last*btc_held*100000).insert -6,"."),
-	((format "%06i", eth_cad.last*eth_held*100000).insert -6,"."),
-	((format "%06i", btg_cad.last*btg_held*100000).insert -6,"."),
-	((format "%06i", btc_cad.last*iota_held*iota_in_btc*100000).insert -6,"."),
-	((format "%06i", ltc_cad.last*ltc_held*100000).insert -6,"."),
-	((format "%06i", costs_to_date*100000).insert -6,"."),
-	((format "%06i", (holdings_in_cad-costs_to_date)*100000).insert -6,"."),
-	((format "%04i", 10000*(holdings_in_cad - costs_to_date)/costs_to_date).insert -3,".")
+	*(
+		[
+			holdings_in_cad,
+			btc_cad.last * btc_held,
+			eth_cad.last * eth_held,
+			btg_cad.last * btg_held,
+			btc_cad.last * iota_held * iota_in_btc,
+			ltc_cad.last * ltc_held,
+			bch_cad.last * bch_held,
+			costs_to_date,
+			holdings_in_cad - costs_to_date
+		].map do |amt|
+			(format "%06i", 100000 * amt).insert -6, "."
+		end
+	),
+	(
+		(
+			format "%04i", 10000 * (holdings_in_cad - costs_to_date) / costs_to_date
+		).insert -3,"."
+	)
