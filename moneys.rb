80f91f6e73261ec33468c304f0a746d419a10f7a
Maciek Nowacki <nowacki@ualberta.ca>
2017-12-14 15:24:36 -0700

    - added bittrex thread - lots of cleanup and other changes

--- moneys.rb
+++ moneys.rb
@@ -8,47 +8,85 @@ Thread.abort_on_exception = true
 qcx_thread = Thread.new do
 	puts "starting quadrigacx [qcx]"
 
-	quadrigacx_templ=URI "https://api.quadrigacx.com/v2/ticker?book=%s"
+	quadrigacx_templ=URI "https://api.quadrigacx.com/v2/ticker?book="
 
 	(
 		%w{ btc_cad eth_cad btg_cad ltc_cad bch_cad }.each_with_object (
 			(Net::HTTP.new quadrigacx_templ.host, quadrigacx_templ.port).tap do |http|
 				puts "qcx: connecting…"
-				http.use_ssl = true
+				http.use_ssl = quadrigacx_templ.scheme == 'https'
 				http.start
 				puts "qcx: …connected"
 			end
 		)
 	).map do |book, (http)|
 		puts "qcx: #{book}"
-		[ book, (http.request Net::HTTP::Get.new (format quadrigacx_templ.to_s, book)) ]
+		[
+			book,
+			(http.request Net::HTTP::Get.new format "%s%s", quadrigacx_templ, book)
+		]
+	end
+end
+
+btx_thread = Thread.new do
+	puts "starting bittrex [btx]"
+
+	bittrex_templ=URI "https://bittrex.com/api/v1.1/public/getticker?market="
+
+	(
+		%w{ BTC-ADA }.each_with_object (
+			(Net::HTTP.new bittrex_templ.host, bittrex_templ.port).tap do |http|
+				puts "btx: connecting…"
+				http.use_ssl = bittrex_templ.scheme == 'https'
+				http.start
+				puts "btx: …connected"
+			end
+		)
+	).map do |book, (http)|
+		puts "btx: #{book}"
+		[
+			book,
+			(http.request Net::HTTP::Get.new format "%s%s", bittrex_templ, book)
+		]
 	end
 end
 
 puts "starting binance [bin]"
 binance_tickers = URI 'https://api.binance.com/api/v1/ticker/allBookTickers'
 
-iota_in_btc = Rational (
-	(
-		JSON.parse (
-			(Net::HTTP.new binance_tickers.host, binance_tickers.port).tap do |http|
-				puts "bin: connecting…"
-				http.use_ssl = true
-				http.start
-				puts "bin: …connected"
-			end.request Net::HTTP::Get.new binance_tickers
-		).read_body
-	).find do |syms|
-		"IOTABTC" == (syms.fetch "symbol")
-	end.fetch "bidPrice"
-)
+iota_in_btc = ((
+	JSON.parse ((
+		Net::HTTP.new binance_tickers.host, binance_tickers.port
+	).tap do |http|
+		puts "bin: connecting…"
+		http.use_ssl = binance_tickers.scheme == 'https'
+		http.start
+		puts "bin: …connected"
+	end.request Net::HTTP::Get.new binance_tickers).read_body
+).find do |syms|
+	"IOTABTC" == (syms.fetch "symbol")
+end.values_at "askPrice","bidPrice").map &:to_r
 
-printf "bin: MIOTA is worth %f BTC\n", iota_in_btc
+puts "bin: MIOTA: askPrice/bidPrice = %f/%f BTC" % iota_in_btc
 
-puts "bin: done. waiting to parse quadrigacx… (qcx_thread status: #{qcx_thread.status})"
+puts "waiting to parse bittrex… (btx_thread status: #{btx_thread.status})"
+# expected:
+# {"success":true,"message":"","result":{"Bid":0.00001184,"Ask":0.00001185,"Last":0.00001185}}
+
+btx_tickers = btx_thread.value.tap do
+	puts "btx: ready\n"
+end.map do |book, ticker_vals|
+	api_res = (JSON.parse ticker_vals.read_body)
+	hilolt = ((api_res.fetch "result").values_at "Bid","Ask","Last").map &:to_r
+	printf "%s Bid/Ask/Last: %i, %i, %i\n", book,
+		*(hilolt.map do |bal| 100000000 * bal end)
+	[ book, hilolt ]
+end
+
+puts "waiting to parse quadrigacx… (qcx_thread status: #{qcx_thread.status})"
 
 qcx_tickers = qcx_thread.value.tap do
-	puts "qcx: done\n"
+	puts "qcx: ready\n"
 end.map do |book, ticker_vals|
 	hilolt = ((JSON.parse ticker_vals.read_body).values_at "high","low","last").map &:to_r
 	puts "#{book} high/low/last: " + ((hilolt.map &:to_f).join ", ")
@@ -57,8 +95,14 @@ end
 
 btc_cad, eth_cad, btg_cad, ltc_cad, bch_cad = qcx_tickers.map &:last
 
-costs_to_date = ARGV[0..-7].reduce 0 do |s,c| s += c.to_r end
-iota_held, btg_held, btc_held, eth_held, ltc_held, bch_held = (-6..-1).map do |idx| Rational ARGV[idx], 100000000 end
+ada_in_btc, = btx_tickers.map &:last
+
+costs_to_date = ARGV[0..-8].reduce 0 do |s,c| s += c.to_r end
+
+iota_held, btg_held, btc_held, eth_held, ltc_held, bch_held, ada_held =
+	(-7..-1).map do |idx|
+		Rational ARGV[idx], 100000000
+	end
 
 holdings_in_cad =
 	btc_cad.last * btc_held +
@@ -66,13 +110,14 @@ holdings_in_cad =
 	btg_cad.last * btg_held +
 	ltc_cad.last * ltc_held +
 	bch_cad.last * bch_held +
-	btc_cad.last * iota_held * iota_in_btc
+	btc_cad.last * iota_held * iota_in_btc.last +
+	btc_cad.last * ada_held * ada_in_btc.last
 
 puts (
 	"done!\n\n" +
 	"Okay, here's how it looksⓇ:\n\n" +
 	"\t* you have CAD $%i.%02i held in total\n" +
-		(%w{ btc eth btg iota ltc bch }.map do |tck| 
+		(%w{ btc eth btg iota ltc bch ada }.map do |tck| 
 			"\t\t* #{tck}:\t$%i.%02i\n"
 		end.join) +
 	"\t* less costs of $%i.%02i…\n" +
@@ -86,9 +131,10 @@ puts (
 			btc_cad.last * btc_held,
 			eth_cad.last * eth_held,
 			btg_cad.last * btg_held,
-			btc_cad.last * iota_held * iota_in_btc,
+			btc_cad.last * iota_held * iota_in_btc.last,
 			ltc_cad.last * ltc_held,
 			bch_cad.last * bch_held,
+			btc_cad.last * ada_held * ada_in_btc.last,
 			costs_to_date,
 			holdings_in_cad - costs_to_date
 		].flat_map do |amt|
