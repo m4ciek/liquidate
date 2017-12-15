131454a7f45db95e684c157425c3ad6133190183
Maciek Nowacki <nowacki@ualberta.ca>
2017-12-15 16:47:20 -0700

    all coin exchange threads are now checked when ready — they notify the main thread via Queue. lots of other cleanup.

--- moneys.rb
+++ moneys.rb
@@ -5,98 +5,93 @@ require 'json'
 
 Thread.abort_on_exception = true
 
-qcx_thread = Thread.new do
-	puts "starting quadrigacx [qcx]"
+cx_q = Queue.new
 
-	quadrigacx_templ=URI "https://api.quadrigacx.com/v2/ticker?book="
+cx_list = [
+	[ "quadrigacx", "qcx", "https://api.quadrigacx.com/v2/ticker?book=%s",
+		%w{ btc_cad eth_cad btg_cad ltc_cad bch_cad },
+		->(api_res) do
+			api_res.values_at "high","low","last"
+		end
+	],
 
-	(
-		%w{ btc_cad eth_cad btg_cad ltc_cad bch_cad }.each_with_object (
-			(Net::HTTP.new quadrigacx_templ.host, quadrigacx_templ.port).tap do |http|
-				puts "qcx: connecting…"
-				http.use_ssl = quadrigacx_templ.scheme == 'https'
-				http.start
-				puts "qcx: …connected"
-			end
-		)
-	).map do |book, (http)|
-		puts "qcx: #{book}"
-		[
-			book,
-			(http.request Net::HTTP::Get.new format "%s%s", quadrigacx_templ, book)
-		]
-	end
-end
-
-btx_thread = Thread.new do
-	puts "starting bittrex [btx]"
-
-	bittrex_templ=URI "https://bittrex.com/api/v1.1/public/getticker?market="
-
-	(
-		%w{ BTC-ADA }.each_with_object (
-			(Net::HTTP.new bittrex_templ.host, bittrex_templ.port).tap do |http|
-				puts "btx: connecting…"
-				http.use_ssl = bittrex_templ.scheme == 'https'
-				http.start
-				puts "btx: …connected"
-			end
-		)
-	).map do |book, (http)|
-		puts "btx: #{book}"
-		[
-			book,
-			(http.request Net::HTTP::Get.new format "%s%s", bittrex_templ, book)
-		]
-	end
-end
-
-puts "starting binance [bin]"
-binance_tickers = URI 'https://api.binance.com/api/v1/ticker/allBookTickers'
-
-iota_in_btc = ((
-	JSON.parse ((
-		Net::HTTP.new binance_tickers.host, binance_tickers.port
-	).tap do |http|
-		puts "bin: connecting…"
-		http.use_ssl = binance_tickers.scheme == 'https'
-		http.start
-		puts "bin: …connected"
-	end.request Net::HTTP::Get.new binance_tickers).read_body
-).find do |syms|
-	"IOTABTC" == (syms.fetch "symbol")
-end.values_at "askPrice","bidPrice").map &:to_r
-
-puts "bin: MIOTA: askPrice/bidPrice = %f/%f BTC" % iota_in_btc
-
-puts "waiting to parse bittrex… (btx_thread status: #{btx_thread.status})"
+# for future development — check "success" and "message" keys in response
 # expected:
 # {"success":true,"message":"","result":{"Bid":0.00001184,"Ask":0.00001185,"Last":0.00001185}}
 
-btx_tickers = btx_thread.value.tap do
-	puts "btx: ready\n"
-end.map do |book, ticker_vals|
-	api_res = (JSON.parse ticker_vals.read_body)
-	hilolt = ((api_res.fetch "result").values_at "Bid","Ask","Last").map &:to_r
-	printf "%s Bid/Ask/Last: %i, %i, %i\n", book,
-		*(hilolt.map do |bal| 100000000 * bal end)
-	[ book, hilolt ]
+	[ "bittrex", "btx", "https://bittrex.com/api/v1.1/public/getticker?market=%s",
+		%w{ BTC-ADA },
+		->(api_res) do
+			(api_res.fetch "result").values_at "Bid", "Ask", "Last"
+		end
+	],
+
+# binance might have a method that returns the ticker for just one book…
+	[ "binance", "bin", "https://api.binance.com/api/v1/ticker/allBookTickers",
+   		%w{ IOTABTC },
+		->(api_res) do
+			api_res.find do |syms|
+				"IOTABTC" == (syms.fetch "symbol")
+			end.values_at "bidPrice", "askPrice"
+		end
+	]
+]
+	
+cx_list.each do |t_args|
+	cx_thr = Thread.new *t_args[0...-1] do |cx_long, cx, uri_templ_str, tickrs|
+		uri_templ = URI uri_templ_str
+
+		http = (Net::HTTP.new uri_templ.host, uri_templ.port).tap do |http|
+			http.use_ssl = uri_templ.scheme == 'https'
+			http.start
+		end
+
+		# improvement: don't wait for all to be finished; instead, return piecemeal in a Queue
+		# (that's a pretty major redesign overall since we'd no longer wait for threads to
+		# complete. We'd be more interested in waiting for data, since that's what we really
+		# care about, anyway.)
+		tickrs.map do |book|
+			[ book, (http.request Net::HTTP::Get.new format uri_templ_str, book) ]
+		end.tap do
+			cx_q.enq [ (Process.clock_gettime Process::CLOCK_MONOTONIC), Thread.current ]
+		end
+	end
+
+	cx_thr.name = t_args[1]
+	cx_thr.thread_variable_set :parser, t_args.last
+	cx_thr.thread_variable_set :long_name, t_args.first
 end
 
-puts "waiting to parse quadrigacx… (qcx_thread status: #{qcx_thread.status})"
+on_your_marks = Process.clock_gettime Process::CLOCK_MONOTONIC
 
-qcx_tickers = qcx_thread.value.tap do
-	puts "qcx: ready\n"
-end.map do |book, ticker_vals|
-	hilolt = ((JSON.parse ticker_vals.read_body).values_at "high","low","last").map &:to_r
-	puts "#{book} high/low/last: " + ((hilolt.map &:to_f).join ", ")
-	[ book, hilolt ]
+tickers = cx_list.size.times.map do |idx|
+	thr_delay, thr = cx_q.deq
+
+	t_now = Process.clock_gettime Process::CLOCK_MONOTONIC
+
+	printf "%s: %s has arrived! order: %i of %i; interactive wait time: %f s; time languishing in queue: %f µs\n",
+		thr.name, (thr.thread_variable_get :long_name), idx.succ, cx_list.size,
+		(t_now - on_your_marks), (t_now - thr_delay) * 1000000
+
+	[
+		thr.name,
+		thr.value.map do |book, ticker_vals|
+			hilolt = (thr.thread_variable_get :parser)[
+				JSON.parse ticker_vals.read_body
+			].map &:to_r
+
+			# XXX this output is crappy. Perhaps it should differ per-currency. Whatever…
+			puts "\t#{book}: " + ((hilolt.map &:to_f).join ", ")
+			[ book, hilolt ]
+		end
+	]
 end
 
-btc_cad, eth_cad, btg_cad, ltc_cad, bch_cad = qcx_tickers.map &:last
+btc_cad, eth_cad, btg_cad, ltc_cad, bch_cad = (tickers.assoc "qcx").last.map &:last
+ada_btc, = (tickers.assoc "btx").last.map &:last
+iota_btc = ((tickers.assoc "bin").last.assoc "IOTABTC").last
+
 cad_cad = [ 1, 1, 1 ]
-
-ada_in_btc, = btx_tickers.map &:last
 
 costs_to_date = ARGV[0..-9].reduce 0 do |s,c| s += c.to_r end
 
@@ -112,12 +107,11 @@ holdings_in_cad =
 	btg_cad.last * btg_held +
 	ltc_cad.last * ltc_held +
 	bch_cad.last * bch_held +
-	btc_cad.last * iota_held * iota_in_btc.last +
-	btc_cad.last * ada_held * ada_in_btc.last
+	btc_cad.last * iota_held * iota_btc.last +
+	btc_cad.last * ada_held * ada_btc.last
 
 puts (
-	"done!\n\n" +
-	"Okay, here's how it looksⓇ:\n\n" +
+	"\nOkay, here's how it looksⓇ:\n\n" +
 	"\t* you have CAD $%i.%02i held in total\n" +
 		(%w{ cad btc eth btg iota ltc bch ada }.map do |tck| 
 			"\t\t* #{tck}:\t$%i.%02i\n"
@@ -134,10 +128,10 @@ puts (
 			btc_cad.last * btc_held,
 			eth_cad.last * eth_held,
 			btg_cad.last * btg_held,
-			btc_cad.last * iota_held * iota_in_btc.last,
+			btc_cad.last * iota_held * iota_btc.last,
 			ltc_cad.last * ltc_held,
 			bch_cad.last * bch_held,
-			btc_cad.last * ada_held * ada_in_btc.last,
+			btc_cad.last * ada_held * ada_btc.last,
 			costs_to_date,
 			holdings_in_cad - costs_to_date
 		].flat_map do |amt|
